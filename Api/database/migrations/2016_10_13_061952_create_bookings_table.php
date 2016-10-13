f2732b04efc5d65a92839982ca87669949f24ca7
Robin De Herdt <rdh_robin@hotmail.com>
2016-10-13 10:01:14 +0200

    Merge fix

---
+++ Api/database/migrations/2016_10_13_061952_create_bookings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->double('price');
+            $table->datetime('date');
+            $table->timestamps();
+
+            $table->integer('host_id')->unsigned();
+            $table->foreign('host_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+}
