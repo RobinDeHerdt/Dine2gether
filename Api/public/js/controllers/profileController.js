24a1dc5f3624b211d6ada89f12757b6a846b039c
Robin De Herdt <rdh_robin@hotmail.com>
2016-11-05 14:47:32 +0100

    Profielpagina 'design'

--- Api/public/js/controllers/profileController.js
+++ Api/public/js/controllers/profileController.js
@@ -1,13 +1,15 @@
-d2gApp.controller("profileController", function () {
+d2gApp.controller("profileController", function (loginService) {
 	var vm = this;
+	var loginSvc = loginService;
 
-	function getProfile()
-	{
-
+	function loadUser () {
+		loginSvc.getUser().then(function (data) {
+				console.log(data);
+			});
 	}
 
 	function _init() {
-		getCurrentBooking();
+		loadUser();
 	}
 	_init();
 });
