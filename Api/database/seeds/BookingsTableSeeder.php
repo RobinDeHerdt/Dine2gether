61ed05606aba7276557df421e06b44bf19bbaaaa
Sharon Meeus <sharon.meeus@gmail.com>
2016-11-06 20:43:42 +0100

    busy with requests

--- Api/database/seeds/BookingsTableSeeder.php
+++ Api/database/seeds/BookingsTableSeeder.php
@@ -17,6 +17,7 @@ class BookingsTableSeeder extends Seeder
         DB::table('bookings')->insert([
             'title' => 'Home cooked vegan meal',
         	'date' => '2016/12/14 19:00:00',
+            'max_guests' => 5
             'street_number' => 'Vlinderstraat 10',
             'postalcode' => 2220,
             'city' => 'Heist-op-den-Berg',
@@ -27,6 +28,7 @@ class BookingsTableSeeder extends Seeder
         DB::table('bookings')->insert([
             'title' => 'Vegan meal',
         	'date' => '2016/12/14 17:30:00',
+            'max_guests' => 10
             'street_number' => 'Liersesteenweg 201A',
             'postalcode' => 2000,
             'city' => 'Antwerpen',
@@ -37,6 +39,7 @@ class BookingsTableSeeder extends Seeder
         DB::table('bookings')->insert([
             'title' => 'Italian home cooking',
         	'date' => '2016/12/14 18:00:00',
+            'max_guests' => 4
             'street_number' => 'Boekenstraat 180',
             'postalcode' => 5000,
             'city' => 'Hasselt',
@@ -47,6 +50,7 @@ class BookingsTableSeeder extends Seeder
         DB::table('bookings')->insert([
             'title' => 'Sexy salsas',
         	'date' => '2016/12/14 16:30:00',
+            'max_guests' => 3
             'street_number' => 'Landweg 1',
             'postalcode' => 3999,
             'city' => 'Bavikhove',
