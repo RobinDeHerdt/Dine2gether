de86d08a86b356b331e01c643bc525cf1ce73d02
Robin De Herdt <robindh95@gmail.com>
2017-07-29 18:49:12 +0200

    Fix all image sources

--- Api/database/seeds/DishImagesTableSeeder.php
+++ Api/database/seeds/DishImagesTableSeeder.php
@@ -12,42 +12,42 @@ class DishImagesTableSeeder extends Seeder
     public function run()
     {
         DB::table('dish_images')->insert([
-            'image_url' => 'foodImage1.jpg',
+            'image_url' => 'images/foodImage1.jpg',
             'dish_id' => 1,
         ]);
 
         DB::table('dish_images')->insert([
-            'image_url' => 'foodImage2.jpg',
+            'image_url' => 'images/foodImage2.jpg',
             'dish_id' => 2,
         ]);
 
         DB::table('dish_images')->insert([
-            'image_url' => 'foodImage3.jpg',
+            'image_url' => 'images/profile/foodImage3.jpg',
             'dish_id' => 3,
         ]);
 
         DB::table('dish_images')->insert([
-            'image_url' => 'foodImage4.jpg',
+            'image_url' => 'images/foodImage4.jpg',
             'dish_id' => 4,
         ]);
 
         DB::table('dish_images')->insert([
-            'image_url' => 'foodImage5.jpg',
+            'image_url' => 'images/foodImage5.jpg',
             'dish_id' => 5,
         ]);
 
         DB::table('dish_images')->insert([
-            'image_url' => 'foodImage6.jpg',
+            'image_url' => 'images/foodImage6.jpg',
             'dish_id' => 6,
         ]);
 
         DB::table('dish_images')->insert([
-            'image_url' => 'foodImage7.jpg',
+            'image_url' => 'images/foodImage7.jpg',
             'dish_id' => 7,
         ]);
 
         DB::table('dish_images')->insert([
-            'image_url' => 'foodImage1.jpg',
+            'image_url' => 'images/foodImage1.jpg',
             'dish_id' => 8,
         ]);
     }
--- Api/database/seeds/UsersTableSeeder.php
+++ Api/database/seeds/UsersTableSeeder.php
@@ -13,43 +13,6 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        /*factory(App\User::class, 50)->create();
-
-        DB::table('users')->insert([
-            'first_name' => 'Robin',
-            'last_name' => 'De Herdt', 
-            'email' => 'robin.deherdt@student.kdg.be',
-            'password' => bcrypt('123456'),
-        ]);
-
-        DB::table('users')->insert([
-            'first_name' => 'Sharon',
-            'last_name' => 'Meeus',
-            'email' => 'sharon.meeus@student.kdg.be',
-            'password' => bcrypt('azerty'),
-        ]);
-
-        DB::table('users')->insert([
-            'first_name' => 'Bob',
-            'last_name' => 'Bouwman',
-            'email' => 'bob.bouwman@kdg.be',
-            'password' => bcrypt('azerty'),
-        ]);
-
-        DB::table('users')->insert([
-            'first_name' => 'Hillary',
-            'last_name' => 'Clinton',
-            'email' => 'hillary@secretemailserver.home',
-            'password' => bcrypt('qwerty'),
-        ]);
-
-        DB::table('users')->insert([
-            'first_name' => 'Donald',
-            'last_name' => 'Trump',
-            'email' => 'yourefired@trump.com',
-            'password' => bcrypt('qwerty'),
-        ]);*/
-        
         Model::unguard();
 
         DB::table('users')->delete();
@@ -59,7 +22,7 @@ class UsersTableSeeder extends Seeder
                     'first_name' => 'Robin',
                     'last_name' => 'De Herdt', 
                     'email' => 'robin.deherdt@student.kdg.be',
-                    'image' => 'friendseating.jpg',
+                    'image' => 'images/profile/friendseating.jpg',
                     'street_number' => 'Vlinderstraat 10',
                     'city' => 'Heist-op-den-Berg',
                     'postalcode' => '2220',
@@ -69,7 +32,7 @@ class UsersTableSeeder extends Seeder
                 [
                     'first_name' => 'Sharon',
                     'last_name' => 'Meeus',
-                    'image' => 'friendseating.jpg',
+                    'image' => 'images/profile/friendseating.jpg',
                     'street_number' => 'Molenstraat 123',
                     'city' => 'Olen',
                     'postalcode' => '2000',
